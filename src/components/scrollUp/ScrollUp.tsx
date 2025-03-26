547149b477831c880a20f8f55a727d0e4288abc7
unknown <voronine@ukr.net>
2025-03-26 11:51:07 +0200

    scroll

--- src/components/scrollUp/ScrollUp.tsx
+++ src/components/scrollUp/ScrollUp.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { setActiveNav } from '../../features/header/slice'
 import styles from './ScrollUp.module.scss'
@@ -6,27 +6,29 @@ import styles from './ScrollUp.module.scss'
 const ScrollUp: React.FC = () => {
   const dispatch = useDispatch()
 
-  const hendlerClick = (): void => {
+  const handlerClick = (): void => {
     dispatch(setActiveNav('#home'))
   }
 
-  window.addEventListener('scroll', () => {
-    const scrollUp = document.querySelector('#scrollUp')
-
-    if (window.pageYOffset >= 560) {
-      scrollUp?.classList.add(`${styles.showScroll}`)
-    } else {
-      scrollUp?.classList.remove(`${styles.showScroll}`)
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollUp = document.querySelector('#scrollUp')
+      if (window.pageYOffset >= 560) {
+        scrollUp?.classList.add(styles.showScroll)
+      } else {
+        scrollUp?.classList.remove(styles.showScroll)
+      }
     }
-
-  })
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
   return (
     <a
       className={styles.scrollUp}
       id="scrollUp"
       href="#home"
-      onClick={hendlerClick}>
+      onClick={handlerClick}>
       <i className={`${styles.scrollIcon} uil uil-arrow-up`} />
     </a>
   )
